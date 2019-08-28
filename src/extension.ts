8859acdb5bcf7efc88770d9aa842d114ace7ec43
yu qi <570079893@qq.com>
2019-08-28 17:22:18 +0800

    update README

--- src/extension.ts
+++ src/extension.ts
@@ -26,14 +26,14 @@ export function activate(context: ExtensionContext) {
 			});
 
 
-			// grab a random port.
 			server.listen(() => {
-				// Start the child java process
 				let options = { cwd: workspace.rootPath };
+				let jarPath = workspace.getConfiguration("rubysonar").jarPath;
+				console.log(jarPath);
 
 				let args = [
 					'-jar',
-				  '/Users/frontier/rubysonar/target/rubysonar-0.1-SNAPSHOT-jar-with-dependencies.jar',
+					jarPath, 
 					server.address().port.toString()
 				];
 
@@ -50,30 +50,22 @@ export function activate(context: ExtensionContext) {
 		});
 	};
 
-	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
 		documentSelector: [{ scheme: "file", language: "ruby" }],
 		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
 			configurationSection: 'languageServerExample',
-			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher("{**/*.rb}")
 		}
 	}
 
-	// Create the language client and start the client.
 	let disposable = new LanguageClient('languageServerExample', 'Language Server Example', createServer, clientOptions).start();
 
-	// Push the disposable to the context's subscriptions so that the 
-	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
 }
 
 
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
